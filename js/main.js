5c36dd048b6326c9526501b639c0843d246689eb
Evgeny67 <e.hromtsov@gmail.com>
2019-10-17 00:23:41 +0300

    добавление функции shuffle

--- js/main.js
+++ js/main.js
@@ -5,49 +5,51 @@ maps.classList.remove('map--faded');
 
 var keksItem = document.querySelector('.map__pins');
 
-var avatar = ('img/avatars/user01.png, img/avatars/user02.png, img/avatars/user03.png, img/avatars/user04.png, img/avatars/user05.png, img/avatars/user06.png, img/avatars/user07.png, img/avatars/user08.png');
-var title = ('Новая метка объявления');
-var address = ('{{location.x}}, {{location.y}}');
-var price = Math.random(0, 99999);
-var type = ('palace, flat, house, bungalo');
-var rooms = Math.random(0, 3);
-var guests = Math.random(0, 2);
-var checkin = ('12 : 00, 13 : 00, 14 : 00');
-var checkout = ('12 : 00, 13 : 00, 14 : 00');
+var title = ['Новая метка объявления'];
+var checkin = ['12 : 00', '13 : 00', '14 : 00'];
+var checkout = ['12 : 00', '13 : 00', '14 : 00'];
+var price = 'min: 1000, max: 100000';
+var description = ['строка с описанием'];
+var rooms = 'min: 1, max: 10';
+var guests = 'min: 1, max: 10';
+var address = '600, 350';
+var type = ['palace', 'flat', 'house', 'bungalo'];
 var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var description = ('строка с описанием');
 var photos = [
   'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
 ];
-// var X = Math.random();
-var Y = Math.random(130, 630);
-// var location = Math.random(X,Y);
+// var location;
 
-var KEKSO_BOOKING = []
+function getRandomMinMax(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-for (var i = 0; i < 8; i++) {
-  KEKSO_BOOKING.push({
-    avatar: 'img src = "img/avatars/user01.png"',
-    title: 'alt = "Метка объявления"',
-    // address,
-    // price,
-    // type,
-    // rooms,
-    // guests,
-    // checkin,
-    // checkout,
-    // features:
-    // description
-    // photos:
-    location: 'left: 200px; top: 400px;'
-  });
-};
+var avatarsList = [];
 
-KEKSO_BOOKING.forEach(function (user) {
-  var keksElement = keksItem.cloneNode(true);
-  // keksElement.querySelector('.map__pins').text = user.title;
-  keksElement.querySelector('.map__pin--main').style = user.location;
-  keksElement.querySelector('.map__pin--main').img = user.avatar;
-});
+function getAvatars() {
+  for (var i = 0; i < 8; i++) {
+    avatarsList[i] = 'img/avatars/user' + i + '.png';
+  }
+}
+
+function shuffle(arr) {
+  var copyArr = arr.slice(0);
+  for (var i = copyArr.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var value = copyArr[i];
+    copyArr[i] = copyArr[j];
+    copyArr[j] = value;
+  }
+  return copyArr;
+}
+
+console.log(shuffle());
+
+// avatarsList.forEach(function (user) {
+//  var keksElement = keksItem.cloneNode(true);
+//  keksElement.querySelector('.map__pins').text = user.title;
+//  keksElement.querySelector('.map__pin').style = user.location;
+//  keksElement.querySelector('.map__pin').text = user.type;
+// });
