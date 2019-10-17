7afd35d72b3c195e73e0ee71abc66c17232a0482
Evgeny67 <e.hromtsov@gmail.com>
2019-10-17 14:53:59 +0300

    исправление замечаний в личном проекте v.2

--- js/main.js
+++ js/main.js
@@ -5,47 +5,42 @@ maps.classList.remove('map--faded');
 
 var keksItem = document.querySelector('.map__pins');
 
-var title = ['Новая метка объявления'];
-var checkin = ['12 : 00', '13 : 00', '14 : 00'];
-var checkout = ['12 : 00', '13 : 00', '14 : 00'];
-var price = 'min: 1000, max: 100000';
-var description = ['строка с описанием'];
-var rooms = 'min: 1, max: 10';
-var guests = 'min: 1, max: 10';
-var address = '600, 350';
-var type = ['palace', 'flat', 'house', 'bungalo'];
+var title = 'Новая метка объявления';
+var checkins = ['12 : 00', '13 : 00', '14 : 00'];
+var checkouts = ['12 : 00', '13 : 00', '14 : 00'];
+var price = window.utils.getRandomPrice();
+var description = 'строка с описанием';
+var rooms = window.utils.getRandomRooms();
+var guests = window.utils.getRandomGuests();
+var address = '{{location.x }}, {{location.y}}';
+var types = ['palace', 'flat', 'house', 'bungalo'];
 var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var photos = [
   'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
 ];
-// var location;
-
-function getRandomMinMax(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+var locationX = window.utils.getRandomX();
+var locationY = window.utils.getRandomY();
 
 var avatarsList = [];
 
 function getAvatars() {
   for (var i = 0; i < 8; i++) {
-    avatarsList[i] = 'img/avatars/user' + i + '.png';
+    avatarsList[i] = 'img/avatars/user' + (i < 10 ? '0' : '') + i + '.png';
   }
 }
 
 function shuffle(arr) {
-  var copyArr = arr.slice(0);
-  for (var i = copyArr.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * (i + 1));
-    var value = copyArr[i];
-    copyArr[i] = copyArr[j];
-    copyArr[j] = value;
+  var arr = []; // Например: если написать var arr = ['palace', 'flat', 'house', 'bungalo'], то функция работает
+  for (var i = arr.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1)); // случайный индекс от 0 до i
+    var x = arr[i];
+    arr[i] = arr[j];
+    arr[j] = x;
   }
-  return copyArr;
+  return arr;
 }
-
-console.log(shuffle());
 
 // avatarsList.forEach(function (user) {
 //  var keksElement = keksItem.cloneNode(true);
