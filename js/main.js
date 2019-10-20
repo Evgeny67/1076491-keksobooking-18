a46dfc9a72f90854e19f3e7a4d388fe0a61fc225
Evgeny67 <e.hromtsov@gmail.com>
2019-10-20 20:01:55 +0300

    добавил вывод метки на экран

--- js/main.js
+++ js/main.js
@@ -3,16 +3,29 @@
 var maps = document.querySelector('.map');
 maps.classList.remove('map--faded');
 
-var keksItem = document.querySelector('.map__pins');
+var cityMap = document.querySelector('.map__pins');
+var button = document.createElement('button');
+button.classList.add('map__pin');
+button.style.left = window.utils.getRandom(300, 900).toString() + 'px';
+button.style.top = window.utils.getRandom(130, 630).toString() + 'px';
+cityMap.appendChild(button);
+
+var picture = document.createElement('img');
+picture.src = getAvatars();
+picture.style.width = '40' + 'px';
+picture.style.height = '40' + 'px';
+picture.alt = 'метка объявления';
+button.appendChild(picture);
+
 
 var title = 'Новая метка объявления';
 var checkins = ['12 : 00', '13 : 00', '14 : 00'];
 var checkouts = ['12 : 00', '13 : 00', '14 : 00'];
-var price = window.utils.getRandomPrice();
+var price = window.utils.getRandom(1000, 100000);
 var description = 'строка с описанием';
-var rooms = window.utils.getRandomRooms();
-var guests = window.utils.getRandomGuests();
-var address = '{{location.x }}, {{location.y}}';
+var rooms = window.utils.getRandom(1, 100);
+var guests = window.utils.getRandom(1, 3);
+var address = 'locationX, locationY';
 var types = ['palace', 'flat', 'house', 'bungalo'];
 var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var photos = [
@@ -20,31 +33,33 @@ var photos = [
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
 ];
-var locationX = window.utils.getRandomX();
-var locationY = window.utils.getRandomY();
-
-var avatarsList = [];
+var locationX = window.utils.getRandom(300, 900);
+var locationY = window.utils.getRandom(130, 630);
 
 function getAvatars() {
+  var avatars = [];
   for (var i = 0; i < 8; i++) {
-    avatarsList[i] = 'img/avatars/user' + (i < 10 ? '0' : '') + i + '.png';
+    avatars[i] = 'img/avatars/user' + (i < 10 ? '0' : '') + i + '.png';
   }
+
+  return avatars;
 }
 
 function shuffle(arr) {
-  var arr = []; // Например: если написать var arr = ['palace', 'flat', 'house', 'bungalo'], то функция работает
+
   for (var i = arr.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * (i + 1)); // случайный индекс от 0 до i
+    var j = Math.floor(Math.random() * (i + 1));
     var x = arr[i];
     arr[i] = arr[j];
     arr[j] = x;
   }
+
   return arr;
 }
 
-// avatarsList.forEach(function (user) {
-//  var keksElement = keksItem.cloneNode(true);
-//  keksElement.querySelector('.map__pins').text = user.title;
-//  keksElement.querySelector('.map__pin').style = user.location;
-//  keksElement.querySelector('.map__pin').text = user.type;
+// avatars.forEach(function (user) {
+// var avatarsElement = ........cloneNode(true);
+//  avatarsElement.querySelector('.map__pins').numder = user.window.utils.getRandom(price);
+//  avatarsElement.querySelector('.map__pin').style = user.location;
+//  avatarsElement.querySelector('.map__pin').text = user.type;
 // });
