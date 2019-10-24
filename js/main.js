e85e281a15c5670adc3c80884c32a3560f99405b
Evgeny67 <e.hromtsov@gmail.com>
2019-10-24 14:34:48 +0300

    начал задание с начала выполнил задание 1 и 2

--- js/main.js
+++ js/main.js
@@ -1,81 +1,58 @@
 'use strict';
 
-var maps = document.querySelector('.map');
-maps.classList.remove('map--faded');
-
-var cityMap = document.querySelector('.map__pins');
-var button = document.createElement('button');
-button.classList.add('map__pin');
-// button.style.left = window.utils.getRandom(300, 900).toString() + 'px';
-// button.style.top = window.utils.getRandom(130, 630).toString() + 'px';
-cityMap.appendChild(button);
-
-var picture = document.createElement('img');
-// picture.src = getAvatars();
-picture.style.width = '40' + 'px';
-picture.style.height = '40' + 'px';
-picture.alt = 'метка объявления';
-button.appendChild(picture);
-
-
-var title = 'Новая метка объявления';
-var checkins = ['12 : 00', '13 : 00', '14 : 00'];
-var checkouts = ['12 : 00', '13 : 00', '14 : 00'];
-var price = window.utils.getRandom(1000, 100000);
-var description = 'строка с описанием';
-var rooms = window.utils.getRandom(1, 100);
-var guests = window.utils.getRandom(1, 3);
-var address = 'locationX, locationY';
-var types = ['palace', 'flat', 'house', 'bungalo'];
+var title = ''; // 'Новая метка объявления'
+var checkin = ['12 : 00', '13 : 00', '14 : 00'];
+var checkout = ['12 : 00', '13 : 00', '14 : 00'];
+var description = ''; // 'строка с описанием'
+var type = ['palace', 'flat', 'house', 'bungalo'];
 var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var photos = [
   'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
 ];
-var locationX = window.utils.getRandom(300, 900).toString() + 'px';
-var locationY = window.utils.getRandom(130, 630).toString() + 'px';
-var avatars = shuffle(getAvatars());
+
+var avatarsList = window.utils.shuffle(getAvatars());
 
 function getAvatars() {
   var value = [];
   for (var i = 0; i < 8; i++) {
-    value[i] = 'img/avatars/user' + (i < 10 ? '0' : '') + i + '.png';
+    value[i] = 'img/avatars/user' + (i < 10 ? '0' : '') + (i + 1) + '.png';
   }
 
   return value;
 }
 
-function shuffle(arr) {
-  for (var i = arr.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * (i + 1));
-    var x = arr[i];
-    arr[i] = arr[j];
-    arr[j] = x;
-  }
-
-  return arr;
-}
-
-var avatarsList = [];
+var avatarObjects = [];
 
 for (var i = 0; i < 8; i++) {
-  avatarsList.push({
-    names: avatars[i],
-    locationX: window.utils.getRandom(300, 900).toString() + 'px',
-    locationY: window.utils.getRandom(130, 630).toString() + 'px',
-    guests: window.utils.getRandom(1, 3)
+  var locationX = window.utils.getRandom(300, 900).toString() + 'px';
+  var locationY = window.utils.getRandom(130, 630).toString() + 'px';
+  avatarObjects.push({
+    'author': {
+      'avatar': avatarsList[i]
+    },
+    'offer': {
+      'title': window.utils.getRandomElement(title),
+      'address': locationX + ',' + locationY,
+      'price': window.utils.getRandom(1000, 100000),
+      'type': window.utils.getRandomElement(type),
+      'rooms': window.utils.getRandom(1, 5),
+      'guests': window.utils.getRandom(1, 3),
+      'checkin': window.utils.getRandomElement(checkin),
+      'checkout': window.utils.getRandomElement(checkout),
+      'features': window.utils.getRandomElement(features),
+      'description': window.utils.getRandomElement(description),
+      'photos': window.utils.getRandomElement(photos),
+      'location': {
+        'x': locationX,
+        'y': locationY
+      }
+    }
   });
 }
 
-// avatarsList.forEach(function (elem) {
-// var avatarsElement = cityMap.cloneNode(true);
-// avatarsElement.querySelector('.map') = elem.names;
-// avatarsElement.querySelector('.map__pins') = elem.locarionX;
-// avatarsElement.querySelector('.map__pins') = user.locationY;
-// avatarsElement.querySelector('.map__pins').text = 'new metka';
+// console.log(avatarObjects);
 
-// cityMap.appendChild(avatarsElement);
-// });
-
-
+var cityMap = document.querySelector('section.map');
+cityMap.classList.remove('map--faded');
