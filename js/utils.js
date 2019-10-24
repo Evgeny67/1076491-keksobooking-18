8509e18235cabec4d05e0b83258364b3ac9706d0
Evgeny67 <e.hromtsov@gmail.com>
2019-10-24 14:37:08 +0300

    вынес функцию shuffle в utils

--- js/utils.js
+++ js/utils.js
@@ -6,4 +6,19 @@
     return Math.floor(Math.random() * (max - min) + min);
   };
 
+  window.utils.shuffle = function (arr) {
+    for (var i = arr.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var x = arr[i];
+      arr[i] = arr[j];
+      arr[j] = x;
+    }
+
+    return arr;
+  };
+
+  window.utils.getRandomElement = function (list) {
+    return list[Math.floor(Math.random() * list.length)];
+  };
+
 })();
